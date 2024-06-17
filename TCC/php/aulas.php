01bc50bf2c5eb9d0a428d75c6f8e7e12f4abcd08
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2024-06-17 10:56:53 -0300

    Add files via upload

--- TCC/php/aulas.php
+++ TCC/php/aulas.php
@@ -21,6 +21,44 @@
 
         </div>
     </header>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Competências Socioemocionais:Autogestão
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              As minhas principais competências de Autogestão é:<strong>Determinação, Foco, Responsabilidadee Persistência.</strong>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Competências Socioemocionais:Amabilidade
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                As minhas principais competências de Amabilidade é:<strong>Empatia, Respeito e Confiança.</strong>
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+             Competências Socioemocionais:Interação com os outros e abertura ao novo
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                As minhas principais competências delas é:<strong>Entusiasmo e Curiosidade para aprender.</strong>
+            </div>
+          </div>
+        </div>
+      </div>
 
 <section class="services">
         <h2>Video Aulas</h2>
