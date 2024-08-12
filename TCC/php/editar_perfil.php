8ca902c0798bb88baab524a0acb1ef418f0d590c
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2024-08-12 11:08:34 -0300

    Add files via upload

--- TCC/php/editar_perfil.php
+++ TCC/php/editar_perfil.php
@@ -73,7 +73,27 @@ if ($usuario):
     <input style="color: black;" type="text" id="renda_mensal" name="renda_mensal" value="<?php echo htmlspecialchars($usuario['renda_mensal']); ?>"><br><br>
     <label style="color: white;" for="valor_final">Valor que sobra no Final do mês:</label>
     <input style="color: black;" type="text" id="valor_final" name="valor_final" value="<?php echo htmlspecialchars($usuario['valor_final']); ?>"><br><br>
-    <input style="color: white;" type="submit" value="Salvar">
+    
+    <!-- Novo botão integrado -->
+    <button>
+        <div class="svg-wrapper-1">
+            <div class="svg-wrapper">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="30"
+                    height="30"
+                    class="icon"
+                >
+                    <path
+                        d="M22,15.04C22,17.23 20.24,19 18.07,19H5.93C3.76,19 2,17.23 2,15.04C2,13.07 3.43,11.44 5.31,11.14C5.28,11 5.27,10.86 5.27,10.71C5.27,9.33 6.38,8.2 7.76,8.2C8.37,8.2 8.94,8.43 9.37,8.8C10.14,7.05 11.13,5.44 13.91,5.44C17.28,5.44 18.87,8.06 18.87,10.83C18.87,10.94 18.87,11.06 18.86,11.17C20.65,11.54 22,13.13 22,15.04Z"
+                    ></path>
+                </svg>
+            </div>
+        </div>
+        <span>Save</span>
+    </button>
+
 </form>
 
 <?php
