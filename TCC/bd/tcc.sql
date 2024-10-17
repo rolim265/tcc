89aeb30c41db64c28572b13e4b5c8c74530448f6
Breno Miguel <100931830+brenomiguel@users.noreply.github.com>
2024-10-17 11:34:15 -0300

    Add files via upload

--- TCC/bd/tcc.sql
+++ TCC/bd/tcc.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13/09/2024 às 00:24
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 17-Out-2024 às 16:33
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,38 +24,90 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `aplicativo`
+-- Estrutura da tabela `aplicativo`
 --
 
 CREATE TABLE `aplicativo` (
   `id` int(11) NOT NULL,
   `link` varchar(255) NOT NULL,
   `descricao` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `capitulo_um`
+-- Estrutura da tabela `capitulo`
+--
+
+CREATE TABLE `capitulo` (
+  `id` int(11) NOT NULL,
+  `pergunta` text DEFAULT NULL,
+  `resposta` text DEFAULT NULL,
+  `video_url` varchar(255) DEFAULT NULL,
+  `video_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `capitulo`
+--
+
+INSERT INTO `capitulo` (`id`, `pergunta`, `resposta`, `video_url`, `video_id`) VALUES
+(1, NULL, NULL, 'https://www.youtube.com/watch?v=VpDvjftqeQA', NULL),
+(2, 'Quem faz o boombap perfeito?', 'Grafiteh', 'https://www.youtube.com/watch?v=VpDvjftqeQA', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `capitulo_um`
 --
 
 CREATE TABLE `capitulo_um` (
   `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `link` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nome` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `pergunta` text NOT NULL,
+  `resposta` text NOT NULL,
+  `alternativa_errada1` text NOT NULL,
+  `alternativa_errada2` text NOT NULL,
+  `alternativa_errada3` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `capitulo_um`
+-- Extraindo dados da tabela `capitulo_um`
 --
 
-INSERT INTO `capitulo_um` (`id`, `nome`, `link`) VALUES
-(1, 'teste1', 'https://www.youtube.com/watch?v=4_sOJjYyJrw');
+INSERT INTO `capitulo_um` (`id`, `nome`, `link`, `pergunta`, `resposta`, `alternativa_errada1`, `alternativa_errada2`, `alternativa_errada3`) VALUES
+(1, 'Boombap', 'https://www.youtube.com/watch?v=VpDvjftqeQA', 'Quem fez o boombap perfeito?', 'Grafith', 'Prado', 'Jotape', 'Kant'),
+(2, 'Java', 'https://www.youtube.com/watch?v=ZBKxhnqX-Q0', 'Quem é o professor?', 'Guanabara', 'Jesus', 'Anderson', 'Cintia Pinho');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produtos`
+-- Estrutura da tabela `perguntas`
+--
+
+CREATE TABLE `perguntas` (
+  `id` int(11) NOT NULL,
+  `resposta1` varchar(255) NOT NULL,
+  `resposta2` varchar(255) NOT NULL,
+  `resposta3` varchar(255) NOT NULL,
+  `resposta4` varchar(255) NOT NULL,
+  `respostacorreta` varchar(255) NOT NULL,
+  `pergunta` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `perguntas`
+--
+
+INSERT INTO `perguntas` (`id`, `resposta1`, `resposta2`, `resposta3`, `resposta4`, `respostacorreta`, `pergunta`) VALUES
+(1, 'a', 'b', 'c', 'd', 'b', ''),
+(2, 'Separar o salario em 4, festas, comida, apostas, gasolina', 'Separar em 3, investimento, gastos gerais e festas', 'Separar em 4, investimento, gastos gerais, lazer e fundo de garantia', 'Separar em 3, Aviador, gastos gerais e festas', 'Separar em 4, investimento, gastos gerais, lazer e fundo de garantia', 'Qual das afirmações estão corretas?');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
@@ -64,10 +116,10 @@ CREATE TABLE `produtos` (
   `descricao` varchar(255) NOT NULL,
   `valor` varchar(255) NOT NULL,
   `link` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `produtos`
+-- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `descricao`, `valor`, `link`) VALUES
@@ -78,7 +130,7 @@ INSERT INTO `produtos` (`id`, `nome`, `descricao`, `valor`, `link`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `usuarios`
+-- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -90,71 +142,90 @@ CREATE TABLE `usuarios` (
   `conhecimento` text NOT NULL,
   `renda_mensal` decimal(30,0) NOT NULL,
   `valor_final` decimal(25,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `usuarios`
+-- Extraindo dados da tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `cartao`, `conhecimento`, `renda_mensal`, `valor_final`) VALUES
-(1, 'Gabriel', '159784', '', '', '', 0, 0),
-(9, 'Gabriel Rodrigues Rolim ', '$2y$10$m3tmPacNtWMSmRhdFjVdie.IJ.VUb/1gh/99N7UmwK0bR6IHjJvbK', 'rolim8096@gmail.com', 'débito', 'básico', 8000, 3000);
+(1, 'Gabriel', '159784', '', '', '', '0', '0'),
+(9, 'Gabriel Rodrigues Rolim ', '$2y$10$m3tmPacNtWMSmRhdFjVdie.IJ.VUb/1gh/99N7UmwK0bR6IHjJvbK', 'rolim8096@gmail.com', 'débito', 'básico', '8000', '3000'),
+(10, 'miguel', '$2y$10$PXI1.Qp3wNpy/bFWLtTRGuOTgpDLcE2j10jS47rt.afOL27bsoHlO', 'breno@gmail.com', '', '', '0', '0'),
+(11, 'miguel', '$2y$10$V8AdTAN6Y54L8GcAlReBiePd2safcEyvHG7p5RKjq4p5IwF5W6PnK', 'bre@gmail.com', '', '', '0', '0'),
+(12, 'Triste', '$2y$10$.3B5Xtd/GE22DOF3S4gd4OJfb3Jw0QtEnGhNqpRXeaXulHXUccqL.', 'triste@gmail.com', '', '', '0', '0'),
+(13, 'breno', '$2y$10$DUXN/ISM75Wplo3n9XP5kOaGF38D2tXkefr6dsI3DVG/Jppd26PnK', 'breno@gmail.com', '', '', '0', '0'),
+(14, 'a', '$2y$10$WOBUymEMgE69.uyBEgPYIuuRLmQR3tVSZlHX.W1EziffTF9NPQWmS', 'a@gmail.com', '', '', '0', '0'),
+(15, 'b', '$2y$10$w7H6RVRaWnWMwSmz/YXB4O.dAbLow0njqqpVt9Gd1U7RGwK6N.Iuu', 'b@gmail.com', 'débito', 'básico', '0', '0');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `video`
+-- Estrutura da tabela `video`
 --
 
 CREATE TABLE `video` (
   `id` int(11) NOT NULL,
   `titulo` text NOT NULL,
   `link` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `video`
+-- Extraindo dados da tabela `video`
 --
 
 INSERT INTO `video` (`id`, `titulo`, `link`) VALUES
-(1, 'teste', 'https://www.youtube.com/watch?v=4_sOJjYyJrw');
+(1, 'História da Educação Financeira', 'https://www.youtube.com/watch?v=z2H3H9Wd0d4&t=151s'),
+(2, 'Gestão Inteligente de Finanças Pessoais', 'https://www.youtube.com/watch?v=6fMlzA5LFCI&t=398s');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `aplicativo`
+-- Índices para tabela `aplicativo`
 --
 ALTER TABLE `aplicativo`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `capitulo_um`
+-- Índices para tabela `capitulo`
+--
+ALTER TABLE `capitulo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `capitulo_um`
 --
 ALTER TABLE `capitulo_um`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `produtos`
+-- Índices para tabela `perguntas`
+--
+ALTER TABLE `perguntas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `usuarios`
+-- Índices para tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `video`
+-- Índices para tabela `video`
 --
 ALTER TABLE `video`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -164,10 +235,22 @@ ALTER TABLE `aplicativo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `capitulo`
+--
+ALTER TABLE `capitulo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT de tabela `capitulo_um`
 --
 ALTER TABLE `capitulo_um`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `perguntas`
+--
+ALTER TABLE `perguntas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
@@ -179,13 +262,13 @@ ALTER TABLE `produtos`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de tabela `video`
 --
 ALTER TABLE `video`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
